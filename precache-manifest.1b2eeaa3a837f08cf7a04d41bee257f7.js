3a270bec40e57e5b01c8ca899ae7abe5c281f7c3
PhantomPink <PhantomPink@users.noreply.github.com>
2020-10-21 22:16:15 +0200

    Updates

---
+++ precache-manifest.1b2eeaa3a837f08cf7a04d41bee257f7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b5b1e4d88faf95410104061b4eabbab",
+    "url": "/catofriends/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/catofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8cf03fe2f6fea89b5569",
+    "url": "/catofriends/static/css/main.f60de741.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/catofriends/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catofriends/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cf03fe2f6fea89b5569",
+    "url": "/catofriends/static/js/main.f698ce76.chunk.js"
+  },
+  {
+    "revision": "2b9f823244586a612853",
+    "url": "/catofriends/static/js/runtime-main.6789fec9.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/catofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
